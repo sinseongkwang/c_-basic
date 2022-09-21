63c83c2bf85f4a220821b946a8d14e157a9d061b
sinseongkwang <112991682+sinseongkwang@users.noreply.github.com>
2022-09-21 10:48:27 +0900

    finished chap 02

---
+++ c_workspace/chap02/c_printf_char.c
@@ -0,0 +1,23 @@
+#inclue <stdio.h>
+
+/**
+ * vs code 단축기
+ * Shift + Alt + N : C실행
+ * Shift + Alt + ↓ or ↑ : Line 복사
+ * Alt + ↓ or ↑ : Line 이동
+ * Ctrl + x : Line 삭제
+ * Ctrl + space : 자동완성
+*/
+
+// ' ' : 문자 (character) 'A' , 'B', '!'
+// " " : 문자열(string) "A", "ABC","A!"
+
+// * 문자(' ')의 경우 키보드 제어를 할 때 사용
+int main(void)
+{ 
+    /* code */
+    printf("%c\n", 'A');
+    printf("%s\n", "A");
+    printf("%c은 %s입니다\n", '1', "first");
+    return 0;"
+}
